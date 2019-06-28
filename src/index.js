bd3ce16e1da1aff10b2880a210e24d8bb6f7b93f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-06-28 10:33:36 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <div>Components Need To Be Mounted</div>,
+  <ToggleButton />,
   document.getElementById('root')
 )
