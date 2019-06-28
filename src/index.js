5ba505a3a991d2c85978303caec9cdf340cc9e2f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-06-28 10:33:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ToggleButton from './components/ToggleButton';
+
 ReactDOM.render(
   <ToggleButton />,
   document.getElementById('root')
